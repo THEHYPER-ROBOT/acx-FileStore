a084e563804f058a96daa73ea5265a900da9754b
THEHYPER-ROBOT <THEHYPER1232@gmail.com>
2025-03-30 22:20:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -54,11 +54,11 @@ FORCE_PIC = os.environ.get("FORCE_PIC", "https://files.catbox.moe/7ycud0.jpg")
 
 # Turn this feature on or off using True or False put value inside  ""
 # TRUE for yes FALSE if no 
-TOKEN = True if os.environ.get('TOKEN', "True") == "True" else False 
+TOKEN = True if os.environ.get('TOKEN', "False") == "True" else False 
 SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "modijiurl.com")
 SHORTLINK_API = os.environ.get("SHORTLINK_API", "654415d3d3b9bdbb4e3f97b5bad6d2296d524a46")
 VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 600)) # Add time in seconds
-IS_VERIFY = os.environ.get("IS_VERIFY", "True")
+IS_VERIFY = os.environ.get("IS_VERIFY", "False")
 TUT_VID = os.environ.get("TUT_VID","https://t.me/howtoopen_modijiurls/2")
 
 
